2bff06c33d23a6a977f26046788a0aa5f3e9baa6
Meth831 <DinuX01Ekanayaka@gmail.com>
2025-01-10 12:23:15 +0530

    initialzed the backend repo and create authentication

---
+++ routes/userRouter.js
@@ -0,0 +1,7 @@
+import express from "express";
+import { loginUser, registerUser } from "../controllers/userController.js";
+const userRouter = express.Router();
+userRouter.post("/",registerUser)
+
+userRouter.post("/login",loginUser)
+export default userRouter;
