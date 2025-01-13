5fdcda8bec7f5c1ce5f101289b397d72ef40fe01
Meth831 <DinuX01Ekanayaka@gmail.com>
2025-01-13 22:16:17 +0530

    async await implementation

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -25,20 +25,13 @@ export function addReview(req, res){
     });
 }
 
-export function getReviews(req,res){
+export async function getReviews(req,res){
     const user = req.user;
-     
-    if(user == null || user.role != "admin"){
-        getReviews.find({isApproved : true}).then((reviews)=>{
-            res.json(reviews);
-        })
-        return
-    }
-
-    if(user.role == "admin"){
-        Review.find().then((reviews)=>{
-            res.json(reviews);
-        })
+    try{
+        const reviews = await Review.find();
+        res.json(reviews);
+    }catch(e){
+        res.status(500).json({error:"Review loading failed"});
     }
 }
 
