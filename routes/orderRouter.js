8e3908cc56b318a9f437275bce301cf1f7654e7c
Methmi_Ekanayaka <DinuX01Ekanayaka@gmail.com>
2025-03-10 14:23:08 +0530

    updated OrderRouter,js

--- routes/orderRouter.js
+++ routes/orderRouter.js
@@ -4,5 +4,5 @@ import { createOrder, getQuote } from '../controllers/orderController.js';
 const orderRouter = express.Router();
 
 orderRouter.post("/",createOrder)
-orderRouter.get("/quote",getQuote)
+orderRouter.post("/quote",getQuote)
 export default orderRouter;
